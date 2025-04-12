1a4cee43e2eaf156ce6f02284bd7606587c26a7b
Max <max.lindqvist@ipp.mpg.de>
2025-04-12 23:17:11 +0200

    Check tutorials with black and isort

--- tutorials/tutorial_04.py
+++ tutorials/tutorial_04.py
@@ -10,14 +10,21 @@ c = canvas.Canvas(width=800, ratio=0.5)
 tikz = c.add_tikzfigure(grid=False)
 
 # Add nodes
-tikz.add_node(0, 0, 'A', shape='circle', draw='black', fill='blue', layer=0)
-tikz.add_node(10, 0, 'B', shape='circle', draw='black', fill='blue', layer=0)
-tikz.add_node(10, 10, 'C', shape='circle', draw='black', fill='blue', layer=0)
-tikz.add_node(0, 10, 'D', shape='circle', draw='black', fill='blue', layer=2)
+tikz.add_node(0, 0, "A", shape="circle", draw="black", fill="blue", layer=0)
+tikz.add_node(10, 0, "B", shape="circle", draw="black", fill="blue", layer=0)
+tikz.add_node(10, 10, "C", shape="circle", draw="black", fill="blue", layer=0)
+tikz.add_node(0, 10, "D", shape="circle", draw="black", fill="blue", layer=2)
 
 
-# Add a line between nodes 
-tikz.add_path(['A', 'B', 'C', 'D'], path_actions=['draw', 'rounded corners'], fill='red', opacity=0.5,cycle=True, layer=1)
+# Add a line between nodes
+tikz.add_path(
+    ["A", "B", "C", "D"],
+    path_actions=["draw", "rounded corners"],
+    fill="red",
+    opacity=0.5,
+    cycle=True,
+    layer=1,
+)
 
 raw_tikz = r"""
 \foreach \i in {0, 45, 90, 135, 180, 225, 270, 315} {
@@ -33,11 +40,11 @@ raw_tikz = r"""
 
 tikz.add_raw(raw_tikz)
 
-tikz.add_node(0.5, 0.5, content='Cube', layer=10)
+tikz.add_node(0.5, 0.5, content="Cube", layer=10)
 
 # Generate the TikZ script
 script = tikz.generate_tikz()
 print(script)
-#print(tikz.generate_standalone())
-tikz.compile_pdf('figures/tutorial_04_figure_01.pdf')
-# 
+# print(tikz.generate_standalone())
+tikz.compile_pdf("figures/tutorial_04_figure_01.pdf")
+#
